43aba123c16db6791cd1704747e15d9c4bcd6b4c
ayan shukla <ayanshukla4@gmail.com>
2020-12-04 00:11:37 +0530

    added sidepanel functionality

--- app.js
+++ app.js
@@ -3,11 +3,9 @@ const sidePanel = document.querySelector('.sidepanel');
 const cross = document.querySelector('.cross');
 
 menuBtn.addEventListener('click', () =>{
-    sidePanel.style.transform = 'translateX(0)';
-    document.body.style.overflowY = 'hidden';
+    sidePanel.classList.add('sidepanel-open');
 })
 
 cross.addEventListener('click', () =>{
-    sidePanel.style.transform = 'translate(388px)';
-    // document.body.style.overflow = 'initial';
+    sidePanel.classList.remove('sidepanel-open');
 })
